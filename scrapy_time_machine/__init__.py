c3e1d65b8674961318fb5e9d080dc9978e312696
Luiz Silva <luizfrdasilva@gmail.com>
2022-07-08 09:09:20 -0300

    Update version to 1.0.1 to upload to Pypi

--- scrapy_time_machine/__init__.py
+++ scrapy_time_machine/__init__.py
@@ -1,4 +1,4 @@
 __author__ = """Luiz Francisco Rodrigues da Silva"""
 __email__ = "luizfrdasilva@gmail.com"
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __all__ = ["__version__", "TimeMachineMiddleware"]
